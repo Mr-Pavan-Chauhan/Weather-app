5aa2ac02782f4d9dc49dee7140df46ade8770981
Mr-Pavan-Chauhan <125576309+Mr-Pavan-Chauhan@users.noreply.github.com>
2023-04-23 14:00:42 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "7511c2e69cba62552536f027e931fd41",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
